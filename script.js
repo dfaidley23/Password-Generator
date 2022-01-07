d190c5882c91eea13ba68677a42ab958f088caf9
David faidley <david.faidley23@gmail.com>
2022-01-07 16:29:03 -0700

    added new btn's, made prompts, added more If statements

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
-var characters = '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numbers = '0123456789';
+var special = '!@#$%^&*()';
+var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+var possChar = '';
 
 // Write password to the #password input
 function generatePassword() {
     var password = '';
     var length = (prompt('How long would you like your password to be? Please choose between 8 - 128 characters.'));
     // a while loop with clauses for the length and an error prompt if the length criteria is not met
-    while(length <= 8 || length >= 129) {
+    while (length < 8 || length > 128) {
         alert('Your password must be between 8 and 128 characters long');
-        var length = (prompt('How long would you like your password to be?'));
-        } 
+        length = (prompt('How long would you like your password to be?'));
+    }
 
-    for (var i = 0; i <= length; i++) {
-        password += characters.charAt(
-        Math.floor(Math.random() * characters.length));
-        }
+    if (document.getElementById('upper').checked){
+        possChar += alphabet.toUpperCase();
+    }
+
+    if (document.getElementById('lower').checked){
+        possChar += alphabet;
+    }
+
+    if (document.getElementById('special').checked){
+        possChar += special;
+    }
+
+    if (document.getElementById('numbers').checked){
+        possChar += numbers;
+    }
+
+
+    for (var i = 0; i < length; i++) {
+        password += possChar.charAt(
+            Math.floor(Math.random() * possChar.length));
+    }
+
     return password;
 }
 
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+
+document.querySelector("#copy").addEventListener("click", function() {
+    document.querySelector("#password").select()
+    document.execCommand("copy")
+})
