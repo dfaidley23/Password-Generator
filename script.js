5e700aaf5168227915f9beeb766b425aca95acbc
David faidley <david.faidley23@gmail.com>
2022-01-07 13:19:15 -0700

    first initial push with all code

---
+++ script.js
@@ -0,0 +1,31 @@
+// Assignment Code
+var generateBtn = document.querySelector('#generate');
+var characters = '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+// Write password to the #password input
+function generatePassword() {
+    var password = '';
+    var length = (prompt('How many characters would you like your password to contain?'));
+    // a while loop with clauses for the length and a error prompt if the length criteria is not met
+    while(length <= 8 || length >= 129) {
+        alert('Your password must be between 8 and 128 characters long');
+        var length = (prompt('How long would you like your password to be?'));
+        } 
+
+    for (var i = 0; i <= length; i++) {
+        password += characters.charAt(
+        Math.floor(Math.random() * characters.length));
+        }
+    return password;
+}
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
